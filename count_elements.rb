807dabce15bf285ce619388cea5220a73732c381
makafis <vinnyalfieri@gmail.com>
2015-06-08 09:25:04 -0400

    complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,22 @@
 def count_elements(array)
-  # code goes here
+
+  array_counter = 0
+  
+  return_hash = Hash.new
+  
+    while array_counter < array.length
+      word_count = 0
+      word_count = array.count(array[array_counter])
+
+    
+      #add the array[counter] to an hash with value of word_count
+      
+      return_hash[array[array_counter]] = word_count
+      array_counter += 1
+
+    end 
+
+   return_hash
+  
+  
 end
- 
